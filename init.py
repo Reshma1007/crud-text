bbd3f9e32a804f4bd6b761ad9557588036403c82
Reshma1007 <85442105+Reshma1007@users.noreply.github.com>
2021-06-14 21:51:15 +0530

    Update init.py

--- init.py
+++ init.py
@@ -1,1 +1,11 @@
+import flask_sqlalchemy
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
