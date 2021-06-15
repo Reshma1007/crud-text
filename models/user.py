c48b27384f4d8e67fb690d28edbd28e24de99252
Reshma1007 <85442105+Reshma1007@users.noreply.github.com>
2021-06-15 17:05:04 +0530

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,18 @@
+from models import db
 
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    title = db.Column(db.String(128))
+    content = db.Column(db.String(98))
+
+    def __init__(self, name, title, content):
+        self.name = name
+        self.title = title
+        self.content = content
+
+
+db.create_all()
