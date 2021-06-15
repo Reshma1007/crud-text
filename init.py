a877917267d8ee890e6302fc69cf023bbef929ea
Reshma1007 <85442105+Reshma1007@users.noreply.github.com>
2021-06-15 14:37:01 +0530

    Update init.py

--- init.py
+++ init.py
@@ -1,11 +1,20 @@
-import flask_sqlalchemy
-from flask import Flask, request, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+from config import CONFIG
+from models import db
+
+from handlers.feed_handler import FEED
+
+
+def init_app():
+    def init_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = CONFIG['SQLALCHEMY_DATABASE_URI']
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = CONFIG['SQLALCHEMY_TRACK_MODIFICATIONS']
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    app.register_blueprint(AUTHENTICATION)
+
+    return app
 
