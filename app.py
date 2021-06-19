47824a81aa8054700391695f50c3c0113cacf9c7
Reshma1007 <85442105+Reshma1007@users.noreply.github.com>
2021-06-19 12:53:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+from models import db
+from flask_migrate import Migrate
 from init import init_app
-
-app=init_app()
-db=flask_sqlalchemy.SQLAlchemy
+app = init_app()
 
 
-if __name__ ==  '__main__':
+migrate = Migrate(app, db)
+
+
+
+@app.route('/')
+def welcome():
+    return "Welcome"
+
+
+if __name__ == '__main__':
     app.run(debug=True)
