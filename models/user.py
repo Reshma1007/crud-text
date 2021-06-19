16047891287f488638a3302e1936e62d27bc46f5
Reshma1007 <85442105+Reshma1007@users.noreply.github.com>
2021-06-19 13:36:13 +0530

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 from models import db
 
 
-class User(db.Model):
+class Feed(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -13,6 +13,44 @@ class User(db.Model):
         self.name = name
         self.title = title
         self.content = content
+        db.create_all()
+
+    @staticmethod
+    def get_user(user_id):
+        users = Feed.query.get(user_id)
+        return users
+
+    @staticmethod
+    def get_all_users():
+        users = Feed.query.all()
+        return users
+
+    @staticmethod
+    def create_user(
+            name,
+            title,
+            content
+    ):
+        users = Feed(
+            name=name,
+            title=title,
+            content=content
+        )
+        db.session.add(users)
+        db.session.commit()
+
+    @staticmethod
+    def update_users(_id, name, title, content):
+        users = Feed.query.get(_id)
+        users.name = name
+        users.title = title
+        users.content = content
+        db.session.add(users)
+        db.session.commit()
 
 
-db.create_all()
+    @staticmethod
+    def del_user(_id):
+        users = Feed.query.get_or_404(_id)
+        db.session.delete(users)
+        db.session.commit()
