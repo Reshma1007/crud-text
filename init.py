dd49c1598d1550ee075d22db4a6127173aadb9a6
Reshma1007 <85442105+Reshma1007@users.noreply.github.com>
2021-06-19 13:00:29 +0530

    Update init.py

--- init.py
+++ init.py
@@ -1,20 +1,16 @@
 from flask import Flask
-from flask_migrate import Migrate
+from handlers.feed_handler import FEED
 
 from config import CONFIG
 from models import db
 
-from handlers.feed_handler import FEED
-
 
 def init_app():
-    def init_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = CONFIG['SQLALCHEMY_DATABASE_URI']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = CONFIG['SQLALCHEMY_TRACK_MODIFICATIONS']
     db.init_app(app)
-    migrate = Migrate(app, db)
-    app.register_blueprint(AUTHENTICATION)
+    app.register_blueprint(FEED)
 
     return app
 
